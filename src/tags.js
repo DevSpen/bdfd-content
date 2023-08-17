fcb00a9dd82b0cbd033290900074d90058ead63b
Spen <69215413+DevSpen@users.noreply.github.com>
2023-08-17 19:23:31 -0400

    Create tags.js

---
+++ src/tags.js
@@ -0,0 +1,13 @@
+[{
+  name: "nowork",
+  title: "No Work",
+  description: `
+      We often get told "doesn't work" "not working", although that's not helpful when trying to fix a issue. Instead of saying "doesn't work"/"not working":
+
+      - Show your code if you haven't already.
+      - Provide the error message, if any.
+      - Explain the exact issue, and share any other context needed.
+      
+      Thanks!`,
+  type: "embed"
+}]
